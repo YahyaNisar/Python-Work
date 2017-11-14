e1aefd7d37a97c964222ea9ed67329f05d26b698
YahyaNisar <33593266+YahyaNisar@users.noreply.github.com>
2017-11-14 18:42:19 +0500

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,13 @@
+name = "ada love lace"
+print (name.title())
+
+print(name.upper())
+print(name.lower())
+
+fn= "adaa"
+lastname="love"
+fullname= fn + " " + lastname
+print (fullname)
+
+message = fullname.title()+" " + fn
+print (message)
